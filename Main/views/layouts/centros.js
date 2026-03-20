4012242ada508dcdaefe63f7ec0bddc2f8ce4ab6
Lawder093 <zaydak93@outlook.com>
2026-03-19 23:51:23 -0600

    add new group views and routes

---
+++ Main/views/layouts/centros.js
@@ -0,0 +1,36 @@
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+
+// <meta charset="UTF-8">
+// <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+// <title><%= title %></title>
+// <meta name="description" content="<%= description %>">
+
+// <link rel="stylesheet" href="/Main/public//css/centros.css">
+// <script defer src="/routes/main.js"></script>
+
+// </head>
+
+// <body>
+
+// <%- include('../partials/search.ejs') %>
+
+// <div class="container">
+
+ 
+//   <%- include('../partials/header.ejs') %>
+
+//  <%- include('../partials/g1pNAV.ejs') %>
+ 
+//   <main class="main">
+//     <%- body %>
+//   </main>
+
+//   <%- include('../partials/footer.ejs') %>
+
+// </div>
+
+// </body>
+// </html>
